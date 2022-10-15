61c7f25a38decae5729a6b86810fda6613763a33
imviz <vishnuedappatt@gmail.com>
2022-10-15 22:05:01 +0530

    changes made

--- src/Axios.js
+++ src/Axios.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 // for api calling we use axios
 const  instance=axios.create({
-    // baseURL:'https://moviebackend.tk/',
-    baseURL:'http://127.0.0.1:8000/'
+    baseURL:'https://moviebackend.tk/',
+    // baseURL:'http://127.0.0.1:8000/'
 })
 
 
